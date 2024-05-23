2f1d1f5bca771ac14053eb0d0e1570425e95d675
Misterguppy <emetpark@gmail.com>
2024-05-23 14:59:48 -0400

    asdasd

--- ApplicationRobot.java
+++ ApplicationRobot.java
@@ -1,10 +1,54 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File; 
+
 public class ApplicationRobot{
     public static void main(String []args) throws Exception{
         Robot calum = new Robot();
-        calum.mouseMove(520,120);
+        calum.mouseMove(10,1050);
+        Thread.sleep(100);
         calum.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        calum.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(2000);
+        calum.mouseMove(80,710);
+        calum.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        calum.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(2000);
+        calum.mouseMove(1240,570);
+        Thread.sleep(2000);
+        calum.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        calum.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(2000);
+        calum.mouseMove(1240,950);
+        Thread.sleep(2000);
+        calum.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        calum.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(2000);
+        calum.mouseMove(750,105);
+        Thread.sleep(2000);
+        calum.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        calum.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(2000);
+        calum.mouseMove(750,225);
+        Thread.sleep(2000);
+        calum.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        calum.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(2000);
+        Thread.sleep(2000);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle bounds = new Rectangle(screenSize);
+        BufferedImage image= calum.createScreenCapture(bounds); 
+        File file = new File("myScreenShot.png");
+        ImageIO.write(image,"png", file);
+        System.out.println("A screenshot is captured to " + file.getPath()); 
+
+        calum.mouseMove(1280,80);
+        Thread.sleep(2000);
+        calum.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        calum.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        
         
     }
 }
